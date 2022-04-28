123413d30fc854a7e232927bda5458ce2e9b74da
AIS245 <gaishwarya24@gmail.com>
2022-04-28 20:00:28 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/todo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container border border-primary rounded mt-5'
+    style={{
+      backgroundColor:'black',
+      backgroundSize: 'cover'
+    }}
+    >
+      <Todo/>
+
     </div>
   );
 }
